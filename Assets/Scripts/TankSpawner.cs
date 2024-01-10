a993ade504681db65784a0cac3af290167948460
kiran2701 <gaurdian.kiran@gmail.com>
2024-01-10 14:40:42 +0530

    Tank Spawning

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject TankPrefab;
+
+    private void Start()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit groundHit, LayerMask.GetMask("Ground")))
+            Instantiate(TankPrefab, groundHit.point, Quaternion.identity);
+        else
+            Debug.LogError("Tank Spawn failed!");
+    }
+}
