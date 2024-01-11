42d2f82d222ff212d8ccf77a190e94e5b74c3572
kiran2701 <gaurdian.kiran@gmail.com>
2024-01-11 12:16:39 +0530

    Added MVC pattern for Tank

---
+++ Assets/Scripts/Tank/TankController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController
+{
+    private TankView tankView;
+    private TankModel tankModel;
+
+    public TankController(TankView _tankView, TankModel _tankModel)
+    {
+        tankView = _tankView;
+        tankModel = _tankModel;
+
+        tankModel.SetTankController(this);
+        tankView.SetTankController(this);
+        GameObject.Instantiate(tankView.gameObject);
+    }
+
+}
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject TankPrefab;
+    [SerializeField] private TankView tankView;
 
     private void Start()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit groundHit, LayerMask.GetMask("Ground")))
-            Instantiate(TankPrefab, groundHit.point, Quaternion.identity);
-        else
-            Debug.LogError("Tank Spawn failed!");
+        //if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit groundHit, LayerMask.GetMask("Ground")))
+        //    Instantiate(tankView, groundHit.point, Quaternion.identity);
+        //else
+        //    Debug.LogError("Tank Spawn failed!");
+        CreateTank();
+    }
+
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankView, tankModel);
     }
 }
